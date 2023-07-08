86ee6656230bbaee72ecfc0d0e740ebce630a195
Shved15 <maks.shihiwtsow15@gmail.com>
2023-07-08 19:31:40 +0200

    создание и конфигурация проекта + создание модели пользователя

---
+++ api/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, gender, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    CHOICE_GENDER = (
+        ('M', 'Men'),
+        ('W', 'Women'),
+    )
+
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True,)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=CHOICE_GENDER)
+    avatar = models.ImageField(upload_to='avatars/', default='default.jpg')
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'gender', 'avatar']
+
+    def __str__(self):
+        return self.email
